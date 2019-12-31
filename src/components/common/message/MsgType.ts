b39337334cded2af0d4607133602d90f1075a566
hhz <860601963@qq.com>
2019-12-31 22:54:40 +0800

    规划消息数据

---
+++ src/components/common/message/MsgType.ts
@@ -0,0 +1,6 @@
+export enum MsgType {
+  // 文本信息
+  TXT = 0,
+  // 图片信息
+  IMG = 1,
+}
---
+++ src/components/common/message/SendType.ts
@@ -0,0 +1,8 @@
+export enum SendType {
+  // 系统消息
+  SYS = 0,
+  // 用户消息
+  USER = 1,
+  // 用户好友消息
+  OTHER = 2,
+}
