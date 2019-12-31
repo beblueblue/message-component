856db0a3d1d921902741d3730eab9cfa3ba69876
hhz <860601963@qq.com>
2019-12-31 22:53:32 +0800

    准备UI素材

---
+++ src/components/common/useInfo/index.js
@@ -0,0 +1,15 @@
+
+export default CodeToUseInfo = {
+  0: {
+    name: '用户',
+    profile: '/images/close-up-of-dog.jpg',
+  },
+  1: {
+    name: '好友1',
+    profile: '/images/swan-swimming-in-body-of-water.jpg',
+  },
+  2: {
+    name: '好友2',
+    profile: '/images/black-yellow-bird-perched-on-tree-branch.jpg',
+  },
+}
